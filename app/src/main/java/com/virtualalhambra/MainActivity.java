f5619641f9d860ccf2875f68ddbbc5c8e8b25295
EloyBG <eloybg97@correo.ugr.es>
2019-10-22 13:06:09 +0200

    Boton para seleccion de personaje

--- app/src/main/java/com/virtualalhambra/MainActivity.java
+++ app/src/main/java/com/virtualalhambra/MainActivity.java
@@ -1,7 +1,11 @@
 package com.virtualalhambra;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +14,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    /** Called when the user taps the Send button */
+    public void selectPersonaje(View view) {
+        Intent intent = new Intent(this, SelectPersonaje.class);
+        startActivity(intent);
+    }
+
 }
