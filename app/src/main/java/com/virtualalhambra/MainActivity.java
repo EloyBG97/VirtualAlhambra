9c0c447a2039a46ff1e555010f6537e9a19bcd09
Wasspy <e.laurarabadan@correo.ugr.es>
2019-10-22 14:58:26 +0200

    Añadido la clase CajaFuerte y la opción de pulsar el botón. Añadida nueva actividad (CajaFuerte)

--- app/src/main/java/com/virtualalhambra/MainActivity.java
+++ app/src/main/java/com/virtualalhambra/MainActivity.java
@@ -1,7 +1,10 @@
 package com.virtualalhambra;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /** Called when the user taps the Send button */
+    public void tocaCajaFuerte (View view) {
+        // Do something in response to button
+        Intent intent = new Intent(this, CajaFuerte.class);
+        startActivity(intent);
     }
 }
