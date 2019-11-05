d427e24810ea5cd5c6715eef0a381875417f1b43
EloyBG <eloybg97@correo.ugr.es>
2019-11-05 19:13:11 +0100

    Añadida libreria para realidad aumentada Añadidos requerimiento de permisos de camara

--- app/src/main/java/com/virtualalhambra/MainActivity.java
+++ app/src/main/java/com/virtualalhambra/MainActivity.java
@@ -1,8 +1,12 @@
 package com.virtualalhambra;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 
@@ -12,6 +16,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Permission is not granted
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.CAMERA)) {
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+            } else {
+                // No explanation needed; request the permission
+                requestPermissions(new String[]{Manifest.permission.CAMERA},1);
+
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+
+        }
     }
 
     /** Called when the user taps the Send button */
--- app/src/main/java/com/virtualalhambra/SelectPersonajes.java
+++ app/src/main/java/com/virtualalhambra/SelectPersonajes.java
@@ -1,14 +1,38 @@
 package com.virtualalhambra;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.widget.FrameLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import com.virtualalhambra.selectpersonajes.CameraPreview;
 
 public class SelectPersonajes extends AppCompatActivity {
 
+    private Camera mCamera;
+    private CameraPreview mPreview;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_personajes);
+
+
+        // Create an instance of Camera
+        try {
+            mCamera = Camera.open();
+        } catch (Exception e){
+        }
+        // Create our Preview view and set it as the content of our activity.
+        mPreview = new CameraPreview(this, mCamera);
+        FrameLayout preview = findViewById(R.id.camera_preview);
+        preview.addView(mPreview);
     }
+
 }
+
