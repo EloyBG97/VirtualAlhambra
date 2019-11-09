b989d480384693e705cf2a910e508a2e0957f7b2
EloyBG <eloybg97@correo.ugr.es>
2019-11-09 12:56:32 +0100

    Mejora de gestion de permisos Correccion de icono

--- app/src/main/java/com/virtualalhambra/MainActivity.java
+++ app/src/main/java/com/virtualalhambra/MainActivity.java
@@ -17,28 +17,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
-                != PackageManager.PERMISSION_GRANTED) {
-
-            // Permission is not granted
-            // Should we show an explanation?
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
-                    Manifest.permission.CAMERA)) {
-                // Show an explanation to the user *asynchronously* -- don't block
-                // this thread waiting for the user's response! After the user
-                // sees the explanation, try again to request the permission.
-            } else {
-                // No explanation needed; request the permission
-                requestPermissions(new String[]{Manifest.permission.CAMERA},1);
-
-
-                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
-                // app-defined int constant. The callback method gets the
-                // result of the request.
-            }
-
-        }
     }
 
     /** Called when the user taps the Send button */
--- app/src/main/java/com/virtualalhambra/SelectPersonajes.java
+++ app/src/main/java/com/virtualalhambra/SelectPersonajes.java
@@ -1,13 +1,18 @@
 package com.virtualalhambra;
 
 import android.Manifest;
+import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.hardware.Camera;
 import android.os.Bundle;
-import android.provider.MediaStore;
+import android.widget.EditText;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import com.virtualalhambra.selectpersonajes.CameraPreview;
@@ -20,19 +25,65 @@ public class SelectPersonajes extends AppCompatActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_select_personajes);
+        setContentView(R.layout.activity_select_personajes_good);
 
+
+        boolean permiso = pedirPermiso();
 
         // Create an instance of Camera
         try {
             mCamera = Camera.open();
-        } catch (Exception e){
+        } catch (Exception e) {
+            System.err.print("ERROR");
         }
         // Create our Preview view and set it as the content of our activity.
         mPreview = new CameraPreview(this, mCamera);
-        FrameLayout preview = findViewById(R.id.camera_preview);
-        preview.addView(mPreview);
+
+        if(permiso) {
+            FrameLayout preview = findViewById(R.id.camera_preview);
+            preview.setVisibility((int) 0);
+            preview.addView(mPreview);
+        }
+
+        else {
+            ImageView imageView = findViewById(R.id.imageView3);
+            imageView.setVisibility((int) 0);
+        }
     }
 
+    public boolean pedirPermiso() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Permission is not granted
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.CAMERA)) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+
+
+                builder.setTitle("Informacion de Permisos");
+                builder.setMessage("La camara es necesaria para poder descubrir un nuevo mundo de realidad aumentada");
+                builder.setCancelable(false);
+
+                builder.setNeutralButton("Entendido", new DialogInterface.OnClickListener() {public void onClick(DialogInterface builder, int id) {
+
+                }});
+
+                AlertDialog dialog = builder.create();
+                builder.show();
+
+
+            } else {
+                requestPermissions(new String[]{Manifest.permission.CAMERA},1);
+
+            }
+
+        }
+
+        return ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                == PackageManager.PERMISSION_GRANTED;
+    }
 }
 
