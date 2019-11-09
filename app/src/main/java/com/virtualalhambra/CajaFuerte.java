e67b1360d4c64fbb4009ce21a8b439e006f878e4
Laura Rabadán Ortega <51179427+Wasspy@users.noreply.github.com>
2019-11-09 18:02:53 +0100

    Commit antes de cambiar a Vero

--- app/src/main/java/com/virtualalhambra/CajaFuerte.java
+++ app/src/main/java/com/virtualalhambra/CajaFuerte.java
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
-import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
@@ -13,15 +12,14 @@ import android.view.View;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
 
 import java.util.ArrayList;
 
 public class CajaFuerte extends AppCompatActivity {
 
     private FusedLocationProviderClient fusedLocationClient;
-    private ArrayList<Double> latitud = new ArrayList<Double>();
-    private ArrayList<Double> longitud = new ArrayList<Double>();
+    private ArrayList<Double> latitud = new ArrayList<>();
+    private ArrayList<Double> longitud = new ArrayList<>();
 
     private int cuadrante;
 
